14d5f77c31b7f0ac52395205c26025f9e2ce0c3d
lholznagel <lars.holznagel@outlook.com>
2017-02-07 20:33:46 +0100

    :rocket: init

---
+++ nodejs/koa/index.js
@@ -0,0 +1,8 @@
+var koa = require('koa');
+var app = module.exports = koa();
+
+app.use(function *() {
+  this.body = 'Hello World';
+});
+
+if (!module.parent) app.listen(5001);
---
+++ nodejs/restify/index.js
@@ -0,0 +1,10 @@
+var restify = require('restify');
+
+var server = restify.createServer();
+
+server.get('/', function (req, res, next) {
+  res.send("Hello world");
+  return next();
+});
+
+server.listen(5002);
